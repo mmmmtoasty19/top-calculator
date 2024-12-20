28fb92d82eb12f6a1bbde4f7deb40381c9a4d7a1
Kyle Belanger <Kyleb44@hotmail.com>
2024-12-20 09:47:49 -0500

    add basic layout

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,6 @@
-
+let number1 = 0;
+let number2 = 0;
+let operation = "";
 
 function sum(num1, num2) {
   return num1 + num2;  
@@ -14,4 +16,26 @@ function multiply(num1,num2) {
 
 function divide(num1,num2) {
   return num1 / num2;
+}
+
+function operate(num1,num2,operator) {
+  let value = 0;
+  switch (operator) {
+    case 'add':
+      value = sum(num1,num2);
+      break;
+    case 'sub':
+      value = subtract(num1,num2);
+      break;
+    case 'multi':
+      value = multiply(num1,num2);
+      break;
+    case 'divide':
+      value = divide(num1,num2);
+      break;
+    default:
+      console.log("operator not found")
+      break;
+  }
+  return(value)
 }
