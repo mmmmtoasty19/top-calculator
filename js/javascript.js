2bc45931f8ee81a70b8fd40d5bc23ab3c501a728
Kyle Belanger <Kyleb44@hotmail.com>
2024-12-20 09:10:31 -0500

    add basic math functions

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+
+
+function sum(num1, num2) {
+  return num1 + num2;  
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1,num2) {
+  return num1 * num2;
+}
+
+function divide(num1,num2) {
+  return num1 / num2;
+}
